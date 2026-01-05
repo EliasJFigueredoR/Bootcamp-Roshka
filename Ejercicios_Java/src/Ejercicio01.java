d7845253deb5604ab67462235395f02efe48e600
LAPTOP\EJFR0 <EliasJFigueredo@outlook.com>
2026-01-05 17:19:12 -0300

    feat: Realizado Ejercicio 01

---
+++ Ejercicios_Java/src/Ejercicio01.java
@@ -0,0 +1,29 @@
+public class Ejercicio01 {
+    private int a = 3;
+    private int b = 4;
+
+    public int sumar(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int restar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b)
+    {
+        return a * b;
+    }
+
+    public float dividir(float a, float b)
+    {
+        return a / b;
+    }
+
+    public int resto (int a, int b)
+    {
+        return a % b;
+    }
+}
---
+++ Ejercicios_Java/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Ejercicio 01
+        Ejercicio01 Ej1 = new Ejercicio01();
+
+        System.out.println("Resta de 3-4 = " + Ej1.restar(3,4));
+        System.out.println("Suma de 3+4 = " + Ej1.sumar(3,4));
+        System.out.println("Multiplicación de 3*4 = " + Ej1.multiplicar(3,4));
+        System.out.println("División de 3/4 = " + Ej1.dividir(3,4));
+        System.out.println("Resto de 3%4 = " + Ej1.resto(3,4));
+
+        //Ejercicio 02
+    }
+}
